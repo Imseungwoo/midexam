76ade619c4996c722a19114ed36cf40d80403814
Imseungwoo <133332@naver.com>
2018-04-27 00:02:09 +0900

    exam4

--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -10,10 +10,12 @@ import java.sql.SQLException;
 public class ProductDaoTest {
 
     private ProductDao productDao;
+    private DaoFactory daoFactory;
 
     @Before
     public void setup() {
-        productDao = new ProductDao();
+        daoFactory = new DaoFactory();
+        productDao = daoFactory.getProductDao();
     }
 
     @Test
